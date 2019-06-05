f2a9d243e75ddf902dea34330df2362f517ea916
Muhammad Mansoor <mmansoor@gmail.com>
2019-06-05 00:15:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
 
   res.write('<h1>Hello World</h1>');
   res.write('<br/>\n ' + os.hostname()+'\n');
-  res.write('<br/>Build #1005 - DEV\n');
+  res.write('<br/>Build #1006 - DEV\n');
   res.end('</body></html>\n')
 
 
